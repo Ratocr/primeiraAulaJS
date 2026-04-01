6626579b99768c6da58048edab21aeedf63c2814
R4t0cr <ratocr@gmail.com>
2026-03-31 21:32:05 -0300

    array

--- POO/mercadinho.js
+++ POO/mercadinho.js
@@ -1,152 +1,85 @@
-class Produto{
-    constructor(nome,preco,quantidade){
-        this.nome = nome
-        this.preco = preco
-        this.quantidade = quantidade
-    }
+class Produto {
+  constructor(nome, preco, quantidade) {
+    this.nome = nome,
+      this.preco = preco
+    this.quantidade = quantidade
+  }
 }
 
-class Carrinho{
-     constructor(){
-        this.itens = []
-     }
+class Carrinho {
+  constructor() {
+    this.itens = []
+  }
 
-     adicionarProdutos(produto){
-        this.itens.push()
-         console.log(`${qd}x ${produto.nome} adicionado ao carrinho`);
-     }
+  adicionarProduto(produto) {
+    this.itens.push(produto)
+  }
 
-     listarProdutosNoCarrinho() {
-        console.log("\n--- ITENS NO SEU CARRINHO ---");
-        this.itens.forEach((item, index) => {
-            console.log(`${index + 1}. ${item.nome} | Qtd: ${item.quantidade} | R$ ${item.preco}`);
-})
-     }
-     
+  removerUltimoItem() {
+    this.itens.pop()
+  }
+  contarItens() {
+    return this.itens.length
+  }
 
+  calcularTotal() {
+    let total = 0
+    this.itens.forEach(p => {
+      total += p.preco
+    })
+    return total
+  }
 
- class Mercado{
-    constructor(nome){
-        this.nome = nome
-        this.estoque = []
-    }
- }
+  listaProdutos() {
+    this.itens.forEach(p => {
+      console.log(`${p.nome} - R$ ${p.preco}`)
+    })
+  }
 
+  removerPorNome(nome) {
+    this.itens = this.itens.filter(p => p.nome !== nome)
+  }
+}
 
+class Mercado {
+  constructor() {
+    this.produtos = []
+  }
 
+  adicionarProduto(produto) {
+    this.produtos.push(produto)
+  }
 
+  mostrarDisponiveis() {
+    this.produtos.forEach(p => {
+      console.log(`${p.nome}  - RS$ ${p.preco}  (Estoque: ${p.quantidade})`)
+    })
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
+let p1 = new Produto('Arroz', 20, 10)
+let p2 = new Produto('Feijão', 20, 10)
 
 
+let m = new Mercado()
+
+m.adicionarProduto(p1)
+m.adicionarProduto(p2)
+
+console.log('produtos disponíveis')
+m.mostrarDisponiveis()
+
+let carrinho = new Carrinho
+
+carrinho.adicionarProduto(p1)
+
+console.log('itens no carrinho')
+
+carrinho.listaProdutos()
+
+console.log(`Total: ${carrinho.calcularTotal}`)
 
 
-
-
-
-//  class Produto {
-//     constructor(nome, preco, quantidade) {
-//         this.nome = nome;
-//         this.preco = preco;
-//         this.quantidade = quantidade;
-//     }
-// }
-
-// class Carrinho {
-//     constructor() {
-//         this.itens = [];
-//     }
-   
-//      adicionarProduto(produto, qd) {
-//         if (qd <= produto.quantidade) {
-//           produto.quantidade -= qd;
-//           this.itens.push();
-
-//             console.log(`${qd}x ${produto.nome} adicionado ao carrinho`);
-//         } else {
-//             console.log(`Estoque insuficiente de ${produto.nome}`);
-//         }
-//     }
-    
-//     removerUltimoItem() {
-//         if (this.itens.length > 0) {
-//             const removido = this.itens.pop();
-//             console.log(`Removido do carrinho: ${removido.nome}`);
-//         } else {
-//             console.log("O carrinho já está vazio.");
-//         }
-//     }
-
-//     contarItens() {
-//         return this.itens.length
-//     }
-
-//     calcularTotal() {
-//         let total = 0;
-//         this.itens.forEach(item => {
-//             total += item.preco * item.quantidade;
-//         });
-//         return total;
-//     }
-
-//    
-//         });
-//     }
-// }
-
-
-// class Mercado {
-//     constructor(nome) {
-//         this.nome = nome;
-//         this.estoque = [];
-//     }
-
-//     adicionarAoEstoque(produto) {
-//         this.estoque.push(produto);
-//     }
-
-//         mostrarDisponiveis() {
-//         console.log(`\n--- PRODUTOS DISPONÍVEIS NO ${this.nome.toUpperCase()} ---`);
-//         this.estoque.forEach(produto => {
-//             if (produto.quantidade > 0) {
-//                 console.log(`${produto.nome} - R$ ${produto.preco} (Estoque: ${produto.quantidade})`);
-//             }
-//         });
-//     }
-// }
-
-
-// const meuMercado = new Mercado("Mercadinho");
-
-// const arroz = new Produto('Arroz',15,10)
-// const feijao = new Produto('Feijao',15,10)
-
-// meuMercado.adicionarAoEstoque(arroz)
-// meuMercado.adicionarAoEstoque(feijao)
-
-
-// meuMercado.mostrarDisponiveis()
+console.log(`Quantidade de itens no carrinho ${carrinho.contarItens}`)
