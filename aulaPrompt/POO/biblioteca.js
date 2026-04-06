231a4da72ea112754976cf0fa0d9b43c94572b5d
R4t0cr <ratocr@gmail.com>
2026-04-06 19:37:06 -0300

    array

---
+++ aulaPrompt/POO/biblioteca.js
@@ -0,0 +1,84 @@
+class Livro {
+    constructor(titulo, autor){
+        this.titulo = titulo
+        this.autor = autor
+        this.disponivel = true
+    }
+
+}
+
+class Biblioteca{
+    constructor(){
+    this.livros = []
+    }
+    
+    adicionarLivro(livro){
+        this.livros.push(livro)
+        console.log(`Livro ${livro.titulo} adicionado com sucesso`)
+    }
+    
+    listarLivros(){
+               this.livros.forEach(livros => 
+            console.log(`livro ${livros.titulo} - ${livros.autor}`)
+        )
+    }
+        
+    emprestarLivro(titulo){
+        
+       const emprestimo  = this.livros.find( livro => livro.titulo === titulo)
+       if (emprestimo.disponivel) {
+        emprestimo.disponivel = false;
+        console.log(`Sucesso: Você pegou "${emprestimo.titulo}" emprestado.`);
+    } else {
+        console.log(`O livro "${emprestimo.titulo}" já está emprestado.`);
+    }
+    }
+
+    desvolverLivro(titulo){
+        const emprestimo  = this.livros.find( livro => livro.titulo === titulo)
+       if(emprestimo){
+        emprestimo.disponivel = true
+        console.log(`Você devolveu o livro ${titulo}`)
+       }
+    }  
+
+    contarDisponiveis(){
+       let contador = 0
+       this.livros.forEach(livro => {
+          if (livro.disponivel === true) {
+            contador++
+          }
+        })
+         console.log(`Ainda tem ${contador} livro(s) disponiveil(s)`)
+}
+
+
+    listarLivrosDisponiveis(){
+        this.livros.forEach(livro => {
+            if (livro.disponivel === true) {
+            console.log(`Livro -  ${livro.titulo} - Autor: ${livro.autor}`);
+            
+            }
+        }); 
+    }
+
+}
+
+const l1 = new Livro('Dom Casmurro', 'Machado de Assis');
+const l2 = new Livro('Memórias Póstumas', 'Machado de Assis');
+
+
+const minhaBiblioteca = new Biblioteca();
+
+minhaBiblioteca.adicionarLivro(l1)
+minhaBiblioteca.adicionarLivro(l2)
+
+minhaBiblioteca.listarLivros()
+minhaBiblioteca.emprestarLivro('Dom Casmurro')
+
+minhaBiblioteca.contarDisponiveis()
+minhaBiblioteca.desvolverLivro('Dom Casmurro')
+
+
+minhaBiblioteca.emprestarLivro('Memórias Póstumas')
+minhaBiblioteca.listarLivrosDisponiveis()
